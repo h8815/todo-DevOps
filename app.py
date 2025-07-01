3836aff0018e9a20d90612951c9ca14d8a86d7d3
Himanshu <shimanshu8181@gmail.com>
2025-07-01 12:36:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
