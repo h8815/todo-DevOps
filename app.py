bf2c3622b6e235c47ee31008033b8259a87d1fdb
h8815 <shimanshu8181@gmail.com>
2025-09-26 18:12:45 +0530

    fix URI

--- app.py
+++ app.py
@@ -16,11 +16,10 @@ import os
 # Load environment variables
 load_dotenv()
 secret_key = os.getenv("SECRET_KEY")
-db_url = os.getenv("DATABASE_URL")
 
 # Flask app configuration
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = secret_key
 
